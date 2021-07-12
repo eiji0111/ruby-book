f3cd10713a3cfeb9f8ad4dba600941667950b0a5
eiji0111 <ee111fender@gmail.com>
2021-07-12 21:21:23 +0900

    [Finish]lesson7

--- lib/somethings.rb
+++ lib/somethings.rb
@@ -1,4 +1,45 @@
-text = '私の誕生日は1997年7月17日です。'
+class Hero
+  attr_accessor :name, :hp
 
-p text[/(?<year>\d+)年(?<munth>\d)月/] # 貪欲なマッチ
-p text[/\d+?/] # 控えめなマッチ
+  def initialize(name, hp)
+    @name = name
+    @hp = hp
+  end
+
+  # デフォルトがpublic
+  
+  def bad
+    sulip
+    p "さらに#{name}は毒になった"
+  end
+
+  private 
+  # privateはレシーバを指定できない！！
+  # 子クラス（サブクラス）でも呼び出し可能
+  def sulip
+    new_hp = hp - 5
+    p "#{name}は転んで5のダメージを受けた"
+    p "#{name}はHPが#{new_hp}になった"
+  end
+end
+
+class LittleHero < Hero
+  TOWN = 'first_town'
+  attr_accessor :mp
+
+  def initialize(name, hp, mp)
+    super(name, hp)
+    @mp = mp
+  end
+
+  def item # インスタンスメソッド（インスタンス名.メソッド）
+    @mp += 5
+  end
+
+  
+  def self.magic # クラスメソッド（クラス名.メソッド）
+    p "Fire"
+  end
+end
+
+hero1 = LittleHero.new('eiji', 50, 10)
